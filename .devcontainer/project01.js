9b7d6a518ab7f4eb8047ce86731d9b013b0d662e
VAMPIRE <143064581+THEVIMPIRE@users.noreply.github.com>
2023-08-25 05:02:06 +0000

    data types

---
+++ .devcontainer/project01.js
@@ -0,0 +1,24 @@
+"use strict"; // treate all js code as newer version
+
+//alert(3+3)// we are using node js , not browser
+
+console.log(3
+    +
+    3)// code redabiity should be high
+    
+console.log ("rohit")
+
+//* data type
+// number => 2to poer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standard value
+// undefined =>
+// symbol=> unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
