049f76d053cafd66f9e540c3d67b1b0ab9453290
VAMPIRE <143064581+THEVIMPIRE@users.noreply.github.com>
2023-08-26 06:44:48 +0000

    conversion of the data type

--- .devcontainer/project02.js
+++ .devcontainer/project02.js
@@ -22,5 +22,43 @@ let someNumber =33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//******************operations********//
+
+let value =3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2/2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2%3)
+
+// let str1 ="rohit"
+// let str2 =" raj"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1" +2+2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+"")
+
+// console.log(true+)//not valid
+// console.log(""+)//not valid
+
+let num1, num2, num3
+
+num1= num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter)
